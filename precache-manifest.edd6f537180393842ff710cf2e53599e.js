3d41e4ae78f614b806a81e186f99407d6953f070
Yusuf Bilal <ubahyusuf484@gmail.com>
2022-04-24 10:14:56 +0100

    Updates

---
+++ precache-manifest.edd6f537180393842ff710cf2e53599e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "267a2d571ae8a384b0ade103da35ed94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5a8a9c8746aff0d057",
+    "url": "/static/css/2.75078c1f.chunk.css"
+  },
+  {
+    "revision": "2bcfac9a5fcee2d36f26",
+    "url": "/static/css/main.033576b5.chunk.css"
+  },
+  {
+    "revision": "2c5a8a9c8746aff0d057",
+    "url": "/static/js/2.749a1ed7.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.749a1ed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcfac9a5fcee2d36f26",
+    "url": "/static/js/main.29c37903.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/static/media/get_schwifty.4b4420cf.ttf"
+  }
+]);
